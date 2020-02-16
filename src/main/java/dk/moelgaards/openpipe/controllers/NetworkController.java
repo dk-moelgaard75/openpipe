b8fd2a231e1e89d62e0c835eb5e88ca464220ae6
dk-moelgaard75 <moelgaard75@gmail.com>
2020-02-16 21:41:41 +0100

    Adding json and xml output from networkcontroller with a WebConfig class. Not quite done yet

--- src/main/java/dk/moelgaards/openpipe/controllers/NetworkController.java
+++ src/main/java/dk/moelgaards/openpipe/controllers/NetworkController.java
@@ -1,25 +1,28 @@
 package dk.moelgaards.openpipe.controllers;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class NetworkController {
-
-		/*
-	@RestController
-	@RequestMapping("/node")
-	*/
+	
+	@ResponseBody
+	@RequestMapping(value = "/network", method = RequestMethod.GET,
+					produces = { "application/json", "application/xml" })
+	public String getNetwork(@RequestParam String type,
+							@RequestParam(required = false) String pipeId) {
+	    StringBuilder html = new StringBuilder();
+	    html.append("type:" + type + "<br/>");
+	    if (pipeId != null)
+	    {
+	    	html.append("pipeId:" + pipeId + "<br/>");
+	    }
+		return html.toString();
+	}
+	
+	
 }
--- src/main/java/dk/moelgaards/openpipe/entities/WaterPipeEntity.java
+++ src/main/java/dk/moelgaards/openpipe/entities/WaterPipeEntity.java
@@ -2,12 +2,16 @@ package dk.moelgaards.openpipe.entities;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
 
+@XmlRootElement
 @Entity
 @Table(name="tbl_waterpipe")
 public class WaterPipeEntity  extends PipeEntity {
+	
+	@SuppressWarnings("unused")
 	private WaterPipeEntity() {
-		
+		//empty private constructor ensures a WasteWaterNode isn´t created without a name
 	}
 	public WaterPipeEntity(WaterNodeEntity fromNode, WaterNodeEntity toNode) {
 		super.setFromNode(fromNode);
